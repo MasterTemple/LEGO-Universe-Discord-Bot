756111f7f5fef747529845156955603bd6b8e1e5
MasterTemple <dgmastertemple@protonmail.com>
2022-01-10 16:38:28 -0800

    renamed slash commands folder to camelCase

--- src/setup.ts
+++ src/setup.ts
@@ -8,9 +8,9 @@ export async function updateSlashCommands(client: Client, commands: SlashCommand
 
 export function getSlashCommands(): SlashCommandMap {
   const slashCommands: SlashCommandMap = new Map();
-  for (const fileName of readdirSync(__dirname + '/slash_commands')) {
+  for (const fileName of readdirSync(__dirname + '/slashCommands')) {
     if (!fileName.endsWith('.js')) continue;
-    const command = require(`${__dirname}/slash_commands/${fileName}`).default;
+    const command = require(`${__dirname}/slashCommands/${fileName}`).default;
     slashCommands.set(command.name, command);
   }
   return slashCommands;
