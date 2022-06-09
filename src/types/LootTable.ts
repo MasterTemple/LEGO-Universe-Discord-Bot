5ab665106cca95390e86c3bebf6a4cce8c779e14
MasterTemple <dgmastertemple@protonmail.com>
2022-06-08 23:23:16 -0700

    added /loottable and specific searching

---
+++ src/types/LootTable.ts
@@ -0,0 +1,33 @@
+import { Database } from "sqlite3";
+import { CDClient } from "../cdclient";
+import { ItemSold, LootTableItem, NPCMission } from "../luInterfaces";
+import { explorerDomain } from '../config.json';
+
+export class LootTable extends CDClient {
+  db: Database;
+  id: number;
+  name: string;
+  imageURL: string;
+  loot: LootTableItem[];
+
+  constructor(cdclient: CDClient, id: number) {
+    super();
+    this.db = cdclient.db;
+    this.id = id;
+    this.locale = cdclient.locale;
+  }
+
+  async create(): Promise<void> {
+    this.name = `Loot Table`;
+    await this.addLoot();
+  }
+
+  getURL(id: number = this.id): string {
+    return `${explorerDomain}/objects/loot/table/${id}`;
+  }
+
+  async addLoot(): Promise<void> {
+    this.loot = await this.getItemsInLootTableWithRarity(this.id)
+  }
+
+}
